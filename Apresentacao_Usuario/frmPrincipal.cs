7463efdeba15b0400bb14fcaf0277cd2a6ecd831
Paulo Cesar C Bruno <p_bruno001@hotmail.com>
2019-09-26 10:06:08 -0300

    C#

--- Apresentacao_Usuario/frmPrincipal.cs
+++ Apresentacao_Usuario/frmPrincipal.cs
@@ -16,7 +16,7 @@ using System.Runtime.InteropServices; // 1ª Passo para desabilitar o "X" do Fec
 
 namespace Apresentacao_Usuario
 {
-    public partial class frmPrincipal : Form
+    public partial class frmPrincipal : 
     {
         //======================================================================
         // Estas Variáveis Amarrao a data de expiração da autorização do sistema.
