7fa947e84e01044874d327fb00e578bfdd1a1b3d
Paulo Cesar C Bruno <p_bruno001@hotmail.com>
2022-01-21 20:40:19 -0300

    Alteração da data de Chave

--- Apresentacao_Usuario/Properties/Settings.Designer.cs
+++ Apresentacao_Usuario/Properties/Settings.Designer.cs
@@ -26,7 +26,8 @@ namespace Apresentacao_Usuario.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=HOME;Initial Catalog=Hospital;User ID=sa;Password=Paradoxo22")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-K63Q18Q;Initial Catalog=Hospital;User ID=sa;Password=Paradoxo" +
+            "22")]
         public string String {
             get {
                 return ((string)(this["String"]));
